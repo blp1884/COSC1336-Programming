18db6d09c4261bdb4ed4003af472aa2c0416f380
blp1884 <81374037+blp1884@users.noreply.github.com>
2021-04-01 20:17:04 -0500

    Add files via upload

---
+++ Business Card/BuisnessCard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "A&B's BBQ and Pies" << std::endl;
+    std::cout << "123 BBQ and Pie Dr San Antonio Texas 78245" << std::endl;
+    std::cout << "+12102227743" << std::endl;
+    std::cout << "http://a&bbbqandpies.com/" << std::endl;
+    return 0;
+}
